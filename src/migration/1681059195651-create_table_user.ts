5833264833d5b35a0ebf8b3337b70988f0524601
Rebecca <Rebecca2216158@aluno.faculdadedescomplica.com.br>
2023-04-09 14:18:54 -0300

    feat: insert UserEntity

---
+++ src/migration/1681059195651-create_table_user.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateTableUser1681059195651 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        
+        `) 
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        
+        `)
+    }
+
+}
